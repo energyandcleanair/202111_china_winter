820a2becb991e4c1085259c45cdce9946bfe4257
Hubert Thieriot <hubert.thieriot@gmail.com>
2022-02-02 08:25:52 +0000

    -separating olympics

--- scripts/tracker_deweathering.R
+++ scripts/tracker_deweathering.R
@@ -4,4 +4,6 @@ library(creadeweather)
 
 Sys.setenv("TZ"="Etc/UTC")
 
-creadeweather::deweather(source='mee', poll='pm25', process_id='city_day_mad', output=c("trend"))
+creadeweather::deweather(source='mee', poll='pm25',
+                         process_id='city_day_mad', output=c("trend","anomaly"),
+                         training_end_anomaly="2021-06-01")
--- scripts/tracker_olympics.R
+++ scripts/tracker_olympics.R
@@ -1,4 +1,17 @@
-plot_olympics_anomaly <- function(city, poll, folder){
+if(!require(remotes)){install.packages("remotes"); library(remotes)}
+if(!require(tidyverse)){install.packages("tidyverse"); library(tidyverse)}
+remotes::install_github("energyandcleanair/creadeweather", force=F, upgrade=F, dependencies=F)
+library(creadeweather)
+
+Sys.setenv("TZ"="Etc/UTC");
+
+# Creating result directories
+folder <- {tmp_dir} # Used in rpy2, replaced by a temporary folder
+setwd(folder)
+
+dir.create("results", showWarnings=F)
+
+plot_olympics_anomaly <- function(city, poll, folder, filename){
   
   cities <- rcrea::cities()
   
@@ -23,7 +36,7 @@ plot_olympics_anomaly <- function(city, poll, folder){
   
   ggplot(m.plot) +
     geom_line(aes(date, value, col=value), size=0.6) +
-    # facet_grid(~ location_name) +
+    facet_grid(location_name ~ rcrea::poll_str(poll)) +
     rcrea::theme_crea() +
     geom_hline(yintercept=0) +
     labs(title=sprintf("%s concentration anomaly in %s", rcrea::poll_str(poll), city),
@@ -38,7 +51,11 @@ plot_olympics_anomaly <- function(city, poll, folder){
           panel.grid.minor.x = element_line(color="#DEDEDE", size=0.3)) +
     scale_color_gradientn(colors = chg_colors, guide = "none", limits=c(-maxabs,maxabs))
   
-  ggsave(file.path(folder, tolower(sprintf("olympics_%s_%s.jpg", city, poll))), width=10, height=6)
+  ggsave(file.path(folder, filename), width=10, height=6)
 }
 
 
+plot_olympics_anomaly(city="Zhangjiakou", poll="no2", folder="results", filename="olympics_zhangjiakou_no2.jpg")
+plot_olympics_anomaly(city="Beijing", poll="no2", folder="results", filename="olympics_zhangjiakou_no2.jpg")
+plot_olympics_anomaly(city=c("Beijing","Zhangjiakou"), poll=c("no2","pm25"), folder="results", filename="olympics_beijing_zhangjiakou_no2_pm25.jpg")
+plot_olympics_anomaly(city=c("Beijing","Zhangjiakou"), poll=c("no2"), folder="results", filename="olympics_beijing_zhangjiakou_no2.jpg")
